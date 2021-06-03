64171ab724e2a6128c929d7a1a8d0b9349b096af
Todimu Jenrola <todimujenrola@gmail.com>
2021-06-02 20:19:34 -0400

    Login page

---
+++ Login Firebase/Login Firebase/Login_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_FirebaseApp.swift
+//  Login Firebase
+//
+//  Created by Todimu Jenrola on 2021-05-10.
+//
+
+import SwiftUI
+
+@main
+struct Login_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
