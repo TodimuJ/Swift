b4b16916626d6f7f83c3780db90d10e0b0dbed20
Todimu Jenrola <todimujenrola@gmail.com>
2021-06-05 12:57:24 -0400

    Form page

---
+++ Form/Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Todimu Jenrola on 2021-05-24.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
