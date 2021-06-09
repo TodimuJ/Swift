2e7ea5945ff644803cf2c65d80967e1f864cc537
Todimu Jenrola <todimujenrola@gmail.com>
2021-06-08 21:23:19 -0400

    Navigations

---
+++ ListNav/ListNav/ListNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavApp.swift
+//  ListNav
+//
+//  Created by Todimu Jenrola on 2021-05-23.
+//
+
+import SwiftUI
+
+@main
+struct ListNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoListView()
+        }
+    }
+}
