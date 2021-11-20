82d374768db93f5e4ad71f6ccc3f2b4ceaf7cba3
Todimu Jenrola <todimujenrola@gmail.com>
2021-11-20 10:49:24 -0500

    Simple animation p1

---
+++ Animations1/Animations1/Animations1App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations1App.swift
+//  Animations1
+//
+//  Created by Todimu Jenrola on 2021-11-20.
+//
+
+import SwiftUI
+
+@main
+struct Animations1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations1/Animations1/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Animations1
+//
+//  Created by Todimu Jenrola on 2021-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button(action: {}, label: {
+            Text("Hello")
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
