b9558fbea9752096bef3ce3ace9174aa3d26dbe9
Todimu Jenrola <todimujenrola@gmail.com>
2021-11-20 11:26:18 -0500

    Simple animation p1

--- Animations1/Animations1/ContentView.swift
+++ Animations1/Animations1/ContentView.swift
@@ -17,21 +17,23 @@ struct ContentView: View {
             
             ZStack{
                 
+                LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue, Color.white, Color.red]), startPoint: .leading, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.vertical)
                 Color.gray
-                    .opacity(0.3)
+                    .opacity(0.1)
                     .edgesIgnoringSafeArea(.all)
                 
                 Button(action: self.animation , label: {
                     Image(systemName: isClicked ? "gobackward" : "plus.circle.fill")
                         .resizable()
+                        .foregroundColor(.red)
                         .frame(width: 60, height: 60, alignment: .center)
                         .rotationEffect(isClicked ? .degrees(90) : .degrees(0))
                         .animation(.spring())
                     
                 })
                 
-                Color
-                    .black
+                Color.blue
                     .frame(width: 100, height: 100, alignment: .center)
                     .offset(x: 0, y: isClicked ? (geometry.size.height)/2 - 100 : (geometry.size.height)/2 + 100)
                     .animation(.easeInOut)
