b563b56481edfcba3c50f798ddf1c78f58432c02
Todimu Jenrola <todimujenrola@gmail.com>
2021-12-08 20:09:17 -0500

    iOS 15 dialog button

---
+++ Dialog1/Dialog1/Dialog1App.swift
@@ -0,0 +1,17 @@
+//
+//  Dialog1App.swift
+//  Dialog1
+//
+//  Created by Todimu Jenrola on 2021-12-08.
+//
+
+import SwiftUI
+
+@main
+struct Dialog1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
