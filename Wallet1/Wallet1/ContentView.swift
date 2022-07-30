5408d79e500bb1cb2241d9d9532e5d668ab1a086
TodimuJ <todimujenrola@gmail.com>
2022-07-30 12:27:18 -0400

    Bank Interface

---
+++ Wallet1/Wallet1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wallet1
+//
+//  Created by Todimu Jenrola on 2022-07-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!, again")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
