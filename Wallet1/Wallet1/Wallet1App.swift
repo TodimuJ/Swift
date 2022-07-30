9258d121cc3024e0ccc56f6b74fa54e8881beca3
TodimuJ <todimujenrola@gmail.com>
2022-07-30 12:37:53 -0400

    Bank Wallet

---
+++ Wallet1/Wallet1/Wallet1App.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet1App.swift
+//  Wallet1
+//
+//  Created by Todimu Jenrola on 2022-07-30.
+//
+
+import SwiftUI
+
+@main
+struct Wallet1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
