248e0aea7853e73efa2fd33ce295300305666793
TodimuJ <todimujenrola@gmail.com>
2022-11-10 18:26:18 -0500

    Moive reservation

---
+++ Movie Booking/Movie Booking/Movie_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_BookingApp.swift
+//  Movie Booking
+//
+//  Created by Todimu Jenrola on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Movie_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie Booking/Movie Booking/View/ReserveView.swift
@@ -0,0 +1,11 @@
+//
+//  ReserveView.swift
+//  Movie Booking
+//
+//  Created by Todimu Jenrola on 2022-11-10.
+//
+
+import Foundation
+import SwiftUI
+
+
