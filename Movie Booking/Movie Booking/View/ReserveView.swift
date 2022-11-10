465235216ed20a716a68e14eb1f5eecd913d8d76
TodimuJ <todimujenrola@gmail.com>
2022-11-10 18:45:20 -0500

    Moive reservation

--- Movie Booking/Movie Booking/View/ReserveView.swift
+++ Movie Booking/Movie Booking/View/ReserveView.swift
@@ -8,4 +8,36 @@
 import Foundation
 import SwiftUI
 
+struct ReserveView: View {
+    var body: some View{
+        ScrollView(.vertical, showsIndicators: false, content: {
+            
+            HStack {
+                
+                Button(action: {}, label: {
+                    Image(systemName: "chevron.left")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                })
+                
+                Spacer()
+                
+            }
+            .overlay(
+            
+            Text("Select Seats")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            )
+            .padding()
+        })
+        .background(Color(.gray)).ignoresSafeArea()
+    }
+}
 
+struct ReserveView_Previews: PreviewProvider{
+    static var previews: some View{
+        ReserveView()
+    }
+}
