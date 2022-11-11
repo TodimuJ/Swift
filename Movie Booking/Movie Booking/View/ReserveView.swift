7a394481b69e4173b1c02b7c47248cbddffcc0f4
TodimuJ <todimujenrola@gmail.com>
2022-11-10 19:12:19 -0500

    Moive reservation

--- Movie Booking/Movie Booking/View/ReserveView.swift
+++ Movie Booking/Movie Booking/View/ReserveView.swift
@@ -25,14 +25,32 @@ struct ReserveView: View {
             }
             .overlay(
             
-            Text("Select Seats")
+            Text("Choose Seats")
                 .font(.title2)
                 .fontWeight(.semibold)
                 .foregroundColor(.white)
             )
             .padding()
+            
+            // Get width of screen
+            GeometryReader{reader in
+                let width = reader.frame(in: .global).width
+                
+                Path {path in
+                    // Make a curve for the theatre screen
+                    path.move(to: CGPoint(x:0, y:50))
+                    path.addCurve(to: CGPoint(x:width, y:50), control1: CGPoint(x:width/2, y:0), control2: CGPoint(x:width, y:50))
+                }
+                .stroke(Color.white, lineWidth: 3)
+            }
+            .frame(height: 50)
+            .padding(.top,20)
+            .padding(.horizontal,30)
+            
+            
+            // Create theatre seats
         })
-        .background(Color(.gray)).ignoresSafeArea()
+        .background(Color("reserveBackground"))
     }
 }
 
