053f186902c15f3536d38faadf9c250c16073277
TodimuJ <todimujenrola@gmail.com>
2022-11-13 14:05:01 -0500

    Added new booking time

--- Movie Booking/Movie Booking/Model/MovieStruct.swift
+++ Movie Booking/Movie Booking/Model/MovieStruct.swift
@@ -12,4 +12,4 @@ import SwiftUI
 
 var about = "In Marvel Studios\' \"Black Panther: Wakanda Forever,\" Queen Ramonda (Angela Bassett), Shuri (Letitia Wright), M'Baku (Winston Duke), Okoye (Danai Gurira) and the Dora Milaje (including Florence Kasumba) fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia (Lupita Nyong'o) and Everett Ross (Martin Freeman) and forge a new path for the kingdom of Wakanda."
 var genre = ["action", "adventure", "sci-fi", "thriller"]
-var time = ["9:45", "12:15", "14:30", "15:25", "16:55", "19:15", "21:50"]
+var time = ["9:45", "12:15", "14:30", "15:25", "16:55", "19:15", "21:50", "11:05"]
