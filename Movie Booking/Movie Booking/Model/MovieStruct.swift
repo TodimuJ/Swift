203ab5c1eeca42226625907d0547be55f71064b4
TodimuJ <todimujenrola@gmail.com>
2022-11-13 14:03:41 -0500

    Added trailer thumbnail

--- Movie Booking/Movie Booking/Model/MovieStruct.swift
+++ Movie Booking/Movie Booking/Model/MovieStruct.swift
@@ -10,6 +10,6 @@ import SwiftUI
 
 //Sample Data for each movie
 
-var about = "Queen Ramonda, Shuri, M'Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with Nakia and Everett Ross to forge a new path for their beloved kingdom."
-var genre = ["action", "adventure", "sci-fi", "drama"]
+var about = "In Marvel Studios\' \"Black Panther: Wakanda Forever,\" Queen Ramonda (Angela Bassett), Shuri (Letitia Wright), M'Baku (Winston Duke), Okoye (Danai Gurira) and the Dora Milaje (including Florence Kasumba) fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia (Lupita Nyong'o) and Everett Ross (Martin Freeman) and forge a new path for the kingdom of Wakanda."
+var genre = ["action", "adventure", "sci-fi", "thriller"]
 var time = ["9:45", "12:15", "14:30", "15:25", "16:55", "19:15", "21:50"]
