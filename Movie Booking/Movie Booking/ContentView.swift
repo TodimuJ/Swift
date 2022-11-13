c11a3843d8a752d5e328429b1fdb23f395416577
TodimuJ <todimujenrola@gmail.com>
2022-11-12 21:13:31 -0500

    Updated Content View

--- Movie Booking/Movie Booking/ContentView.swift
+++ Movie Booking/Movie Booking/ContentView.swift
@@ -10,10 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 
-        NavigationView{
+        TabView{
+//            NavigationView{
+                HomeView()
+                        .tabItem{
+                            Label("Home", systemImage:"house.fill")
+                        }
+                        .toolbar(.visible, for: .tabBar)
+//                        .toolbarBackground(Color.yellow, for: .tabBar)
+//                        .toolbar(.visible, for: .tabBar)
+//            }
+            
+            ReserveView()
+            .tabItem {
+                Label("Search", systemImage:"magnifyingglass")
+            }
+            .toolbar(.visible, for: .tabBar)
+            
+            
+            VStack{
+                
+            }
+        .tabItem {
+            Label("Saved", systemImage:"bookmark.fill")
+        }
+        .toolbar(.visible, for: .tabBar)
+            
+            VStack{
+                
+            }
+        .tabItem {
+            Label("Profile", systemImage:"person.fill")
+        }
+        .toolbar(.visible, for: .tabBar)
             
         }
-        ReserveView()
+        .ignoresSafeArea(.all)
+        .accentColor(.black)
+
+//        ReserveView()
     }
 }
 
