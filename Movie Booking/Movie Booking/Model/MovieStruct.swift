519c581dcd926b85cc84c7ad04ea6f8c42c73ee0
TodimuJ <todimujenrola@gmail.com>
2022-11-13 11:08:36 -0500

    Updating movie details

--- Movie Booking/Movie Booking/Model/MovieStruct.swift
+++ Movie Booking/Movie Booking/Model/MovieStruct.swift
@@ -10,6 +10,6 @@ import SwiftUI
 
 //Sample Data for each movie
 
-var about = ""
-var genre = ["action", "adventure", "sci-fi"]
+var about = "Queen Ramonda, Shuri, M'Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T'Challa's death. As the Wakandans strive to embrace their next chapter, the heroes must band together with Nakia and Everett Ross to forge a new path for their beloved kingdom."
+var genre = ["action", "adventure", "sci-fi", "drama"]
 var time = ["9:45", "12:15", "14:30", "15:25", "16:55", "19:15", "21:50"]
