344ffa8573d9ac232480a611c8ee1a7603659781
TodimuJ <todimujenrola@gmail.com>
2022-11-13 11:09:21 -0500

    Updated main render

--- Movie Booking/Movie Booking/ContentView.swift
+++ Movie Booking/Movie Booking/ContentView.swift
@@ -7,48 +7,76 @@
 
 import SwiftUI
 
+class Theme {
+    static func navigationBarColors(background : UIColor?,
+       titleColor : UIColor? = nil, tintColor : UIColor? = nil ){
+        
+        let navigationAppearance = UINavigationBarAppearance()
+        navigationAppearance.configureWithOpaqueBackground()
+        navigationAppearance.backgroundColor = background ?? .clear
+        
+        navigationAppearance.titleTextAttributes = [.foregroundColor: titleColor ?? .black]
+        navigationAppearance.largeTitleTextAttributes = [.foregroundColor: titleColor ?? .black]
+       
+        UINavigationBar.appearance().standardAppearance = navigationAppearance
+        UINavigationBar.appearance().compactAppearance = navigationAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationAppearance
+
+        UINavigationBar.appearance().tintColor = tintColor ?? titleColor ?? .black
+    }
+}
+
+
 struct ContentView: View {
+    
+    init(){
+       Theme.navigationBarColors(background: .clear, titleColor: .white)
+    }
+    
     var body: some View {
 
-        TabView{
-//            NavigationView{
+        NavigationView{
+            TabView{
+                //            NavigationView{
                 HomeView()
-                        .tabItem{
-                            Label("Home", systemImage:"house.fill")
-                        }
-                        .toolbar(.visible, for: .tabBar)
-//                        .toolbarBackground(Color.yellow, for: .tabBar)
-//                        .toolbar(.visible, for: .tabBar)
-//            }
-            
-            ReserveView()
-            .tabItem {
-                Label("Search", systemImage:"magnifyingglass")
-            }
-            .toolbar(.visible, for: .tabBar)
-            
-            
-            VStack{
+                    .tabItem{
+                        Label("Home", systemImage:"house.fill")
+                    }
+//                    .toolbar(.visible, for: .tabBar)
+                //                        .toolbarBackground(Color.yellow, for: .tabBar)
+                //                        .toolbar(.visible, for: .tabBar)
+                //            }
+                
+                VStack{
+                    
+                }
+                    .tabItem {
+                        Label("Search", systemImage:"magnifyingglass")
+                    }
+                    .toolbar(.visible, for: .tabBar)
+                
+                
+                VStack{
+                    
+                }
+                .tabItem {
+                    Label("Saved", systemImage:"bookmark.fill")
+                }
+                .toolbar(.visible, for: .tabBar)
+                
+                VStack{
+                    
+                }
+                .tabItem {
+                    Label("Profile", systemImage:"person.fill")
+                }
+                .toolbar(.visible, for: .tabBar)
                 
             }
-        .tabItem {
-            Label("Saved", systemImage:"bookmark.fill")
+//            .ignoresSafeArea(.all)
+            .accentColor(.black)
         }
-        .toolbar(.visible, for: .tabBar)
-            
-            VStack{
-                
-            }
-        .tabItem {
-            Label("Profile", systemImage:"person.fill")
-        }
-        .toolbar(.visible, for: .tabBar)
-            
-        }
-        .ignoresSafeArea(.all)
-        .accentColor(.black)
 
-//        ReserveView()
     }
 }
 
